55359659ad0598fb29f965a5dedb1f1a1cbf45bf
KarolaineBM <karolainebarrosm@gmail.com>
2023-04-30 14:16:52 -0300

    alterações na page inical + logotipo + colors

--- src/pages/SelecionePage.vue
+++ src/pages/SelecionePage.vue
@@ -1,5 +1,9 @@
 <template>
-  <q-layout>
+  <q-layout class="body">
+    <div class="row justify-center">
+      <img src="~assets/logotipo.png" style="max-width: 16rem" />
+    </div>
+
     <div class="row">
       <div class="col-md col-xs-12 q-pa-md">
         <a href="/hp-quiz" class="textos">
@@ -7,7 +11,7 @@
             class="imagens"
             src="~assets/HP/harrypotter-carousel-1.png"
           ></q-img>
-          <q-card class="q-pa-xs text-center mudanca"
+          <q-card class="q-pa-xs text-center"
             >Qual o seu nivel de potterhead?</q-card
           >
         </a>
@@ -15,7 +19,7 @@
       <div class="col-md col-xs-12 q-pa-md">
         <a href="/sda-quiz" class="textos">
           <q-img class="imagens" src="~assets/SDA/sda-1.jpg"></q-img>
-          <q-card class="q-pa-xs text-center mudanca"
+          <q-card class="q-pa-xs text-center"
             >O quanto você conhce Senhor dos Aneis?</q-card
           >
         </a>
@@ -26,7 +30,7 @@
       <div class="col-md col-xs-12 q-pa-md">
         <a href="/got-quiz" class="textos">
           <q-img class="imagens" src="~assets/GOT/got-1.webp"></q-img>
-          <q-card class="q-pa-xs text-center mudanca"
+          <q-card class="q-pa-xs text-center"
             >Qual é sua casa de Game of Thrones?</q-card
           >
         </a>
@@ -34,9 +38,7 @@
       <div class="col-md col-xs-12 q-pa-md">
         <a href="/vikns-quiz" class="textos">
           <q-img class="imagens" src="~assets/VIKNS/vikns-1.webp"></q-img>
-          <q-card class="q-pa-xs text-center mudanca"
-            >Qual Vinking você seria?</q-card
-          >
+          <q-card class="q-pa-xs text-center">Qual Vinking você seria?</q-card>
         </a>
       </div>
     </div>
@@ -65,14 +67,15 @@ export default defineComponent({
   text-decoration: none;
   color: black;
   font-size: 1rem;
-}
-.mudanca {
   transition: 0.5s;
 }
-.mudanca:hover {
-  background: gray;
-  color: white;
+.textos:hover {
+  color: #ff5757;
+  font-weight: 600;
   font-size: 1.2rem;
   transition: 0.5s;
+}
+.body {
+  background-image: linear-gradient(to top, #38b6ff, #dcdcdc);
 }
 </style>
